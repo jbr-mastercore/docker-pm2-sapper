a529dfb1ae02c6876767356a99cd9b8c4d9f71b6
AaronM <info@aamservices.uk>
2020-02-23 11:51:15 +0000

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@
        {
          name: 'sapper',
          cwd: '/usr/src/app/sapperApp/',
-         script: 'npm run build && npm run start',
+         script: 'npm run dev',
          instances: 1,
          autorestart: true,
          watch: false,
